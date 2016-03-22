a141d29a6787ad7e780a72a58ee622c2cf87bd64
Banner2404 <zhekon2404@gmail.com>
2016-03-22 18:24:28 +0300

    Initial Commit

---
+++ BudgetManager/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BudgetManager
+//
+//  Created by Соболь Евгений on 22.03.16.
+//  Copyright © 2016 Соболь Евгений. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

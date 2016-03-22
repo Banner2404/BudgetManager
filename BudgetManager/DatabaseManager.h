3f4a684343ac8a56fc9e7d6ebdae59c7c7c81e4d
Banner2404 <zhekon2404@gmail.com>
2016-03-22 19:01:17 +0300

    DatabaseManager added

---
+++ BudgetManager/DatabaseManager.h
@@ -0,0 +1,24 @@
+//
+//  DatabaseManager.h
+//  BudgetManager
+//
+//  Created by Соболь Евгений on 22.03.16.
+//  Copyright © 2016 Соболь Евгений. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@interface DatabaseManager : NSObject
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
++ (DatabaseManager*)sharedManager;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end

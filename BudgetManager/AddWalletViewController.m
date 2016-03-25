e07d5c60a4c8fc4a827a755032ea9e1e1bfa29b4
Banner2404 <zhekon2404@gmail.com>
2016-03-25 14:06:44 +0300

    added blank fields check in Add Wallet

--- BudgetManager/AddWalletViewController.m
+++ BudgetManager/AddWalletViewController.m
@@ -52,39 +52,6 @@
     
 }
 
-#pragma mark - Actions
-
-- (IBAction)actionSecureSwitch:(UISwitch *)sender {
-        
-    [self checkSequre];
-    
-}
-
-- (IBAction)actionDoneButton:(UIBarButtonItem *)sender {
-    
-    NSString* name = self.walletNameTextField.text;
-    NSInteger cash = [self.cashMoneyTextLabel.text integerValue];
-    NSInteger bank = [self.bankMoneyTextLabel.text integerValue];
-    BOOL isSecure = self.secureSwitch.isOn;
-    NSString* password = self.passwordTextField.text;
-
-    [[DatabaseManager sharedManager] createWalletWithName:name
-                                                     cash:cash
-                                                     bank:bank
-                                                 security:isSecure
-                                                 password:password];
-    
-    [[DatabaseManager sharedManager] saveContext];
-    
-    MainViewController* mainVC = [[self.navigationController viewControllers] objectAtIndex:0];
-    
-    [mainVC refreshInfo];
-
-    
-    [self.navigationController popViewControllerAnimated:YES];
-
-}
-
 - (BOOL)nameField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
     
     NSCharacterSet* charactersSet = [[NSCharacterSet alphanumericCharacterSet] invertedSet];
@@ -103,6 +70,76 @@
         return NO;
     }
     return YES;
+}
+
+- (void)validCreation{
+    
+    NSString* name = self.walletNameTextField.text;
+    NSInteger cash = [self.cashMoneyTextLabel.text integerValue];
+    NSInteger bank = [self.bankMoneyTextLabel.text integerValue];
+    BOOL isSecure = self.secureSwitch.isOn;
+    NSString* password = self.passwordTextField.text;
+    
+    [[DatabaseManager sharedManager] createWalletWithName:name
+                                                     cash:cash
+                                                     bank:bank
+                                                 security:isSecure
+                                                 password:password];
+    
+    [[DatabaseManager sharedManager] saveContext];
+    
+    MainViewController* mainVC = [[self.navigationController viewControllers] objectAtIndex:0];
+    
+    [mainVC refreshInfo];
+    
+    
+    [self.navigationController popViewControllerAnimated:YES];
+
+    
+}
+
+- (void)showAlertWithTitle:(NSString*) title message:(NSString*) message actionName:(NSString*) actionName{
+    
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:title
+                                                                   message:message
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    UIAlertAction* action = [UIAlertAction actionWithTitle:actionName
+                                                     style:UIAlertActionStyleDefault
+                                                   handler:nil];
+    
+    [alert addAction:action];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+
+    
+}
+
+#pragma mark - Actions
+
+- (IBAction)actionSecureSwitch:(UISwitch *)sender {
+    
+    [self checkSequre];
+    
+}
+
+- (IBAction)actionDoneButton:(UIBarButtonItem *)sender {
+    
+    if ([self.walletNameTextField.text isEqualToString:@""]) {
+        
+        [self showAlertWithTitle:@"Name" message:@"Please enter name for wallet" actionName:@"OK"];
+        
+    }else if (self.secureSwitch.isOn && [self.passwordTextField.text isEqualToString:@""]) {
+        
+        [self showAlertWithTitle:@"Password" message:@"Please enter password for wallet" actionName:@"OK"];
+        
+    }else{
+        
+        [self validCreation];
+        
+    }
+    
+    
 }
 
 #pragma mark - UITextFieldDelegate
